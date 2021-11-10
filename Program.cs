e3f172155122beb400f445d31e48e2617b6dd034
Meenaakshi-Ambati <ambatimeenakshi9@gmail.com>
2021-11-10 12:13:33 +0530

    Intial Commit

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ClassTypes
+{
+    class  Customer
+    {
+        string _firstname;
+        string _lastname;
+
+        public Customer(string firstname, string lastname )
+         {
+            _firstname = firstname;
+            _lastname = lastname;
+         }
+
+          public string PrintFullName()
+           {
+              return _firstname + " " + _lastname;
+           }
+
+           ~Customer()
+           {
+
+
+           }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer c1 = new Customer("Meenaakshi", "Ambati");
+            Console.WriteLine(c1.PrintFullName());
+        }
+    }
+}
